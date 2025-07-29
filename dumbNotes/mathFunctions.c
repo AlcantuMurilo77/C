5403c6009f390cf2922f16c027c844981d4a4ff8
Murilo Alcantu <alcantumurilo@gmail.com>
2025-07-28 20:19:56 -0400

    important stuff of conditions and math simple basic yay

---
+++ dumbNotes/mathFunctions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    float x = 9.3;
+
+    x = sqrt(x);
+
+    x = pow(9, 2);
+
+    x = round(x);
+
+    x = ceil(x); //rounded up
+
+    x = floor(x); // rounded down
+
+    printf("%d\n", x);
+
+
+    return 0;
+}
---
+++ dumbNotes/whatIfBro.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int age = 21;
+
+    if (age >= 18){
+        printf("yeaaa");
+    } else{
+        printf("nooo");
+    }
+
+
+    return 0;
+}
