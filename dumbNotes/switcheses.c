94581aab12d91b9259cf2e7d78e1172a2a859994
Murilo Alcantu <alcantumurilo@gmail.com>
2025-07-28 20:54:54 -0400

    swiitchhh not the nitendo tho >:(

---
+++ dumbNotes/switcheses.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int dayOfWeek = 0;
+
+    switch(dayOfWeek){
+
+        case 1:
+        printf("Its monday\n");
+        break;
+
+        case 2:
+        printf("it is tuesday\n");
+        break;
+
+        case 3:
+        printf("it is wednesday\n");
+
+        case 4:
+        printf("it is thursday\n");
+
+        case 5:
+        printf("it is friday\n");
+
+        case 6:
+        printf("it is saturday\n");
+
+        case 7:
+        printf("it is sunday\n");
+
+        default:
+        printf("well damn it\n");
+
+    }
+
+
+
+    return 0;
+}
